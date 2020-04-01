e1742a70c8acee91ab00dff9116fb17a5a60b2e5
Andris Berzkalns <andris.berzkalns@d8corp.com>
2020-04-01 18:57:42 +0300

    Orientation test

--- src/pages/Demos/DeviceOrientation/DeviceOrientation.js
+++ src/pages/Demos/DeviceOrientation/DeviceOrientation.js
@@ -1,12 +1,36 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const DeviceOrientationDemo = () => {
 
+    const [absolute, setAbsolute] = useState(false);
+    const [alpha, setAlpha] = useState(0);
+    const [beta, setBeta] = useState(0);
+    const [gamma, setGamma] = useState(0);
+
+
+    useEffect(() => {
+
+        window.addEventListener('deviceorientation', handleDeviceOrientation, true);
+        
+        return () => {
+            window.removeEventListener('deviceorientation', handleDeviceOrientation, true);
+        }
+    }, []);
+
+    const handleDeviceOrientation = (event) => {
+        console.log(event);
+        setAbsolute(event.absolute);
+        setAlpha(event.alpha);
+        setBeta(event.beta);
+        setGamma(event.gamma);
+    }
+
 
     return (
-        <div>
-
-        </div>
+            <div className="device-orientation-container">
+                <div className="device-orientation-object" style={{ 'marginLeft': (alpha + 180) + 'px', 'marginTop': (beta + 180) + 'px'}}>
+                </div>
+            </div>
     )
 }
 
